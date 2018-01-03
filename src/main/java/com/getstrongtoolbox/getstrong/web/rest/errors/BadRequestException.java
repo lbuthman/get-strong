4c5dbedc2346a35d0e642235134608b9b7854058
Luke Buthman <lbuthman@gmail.com>
2018-01-03 17:57:46 -0600

    [feat] Add class for throwing bad requests from controller

--- src/main/java/com/getstrongtoolbox/getstrong/web/rest/errors/BadRequestException.java
+++ src/main/java/com/getstrongtoolbox/getstrong/web/rest/errors/BadRequestException.java
@@ -1,4 +1,16 @@
 package com.getstrongtoolbox.getstrong.web.rest.errors;
 
-public class BadRequestException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+    private final String description;
+    private final String entityName;
+
+    public BadRequestException(String description, String entityName) {
+        this.description = description;
+        this.entityName = entityName;
+    }
 }
