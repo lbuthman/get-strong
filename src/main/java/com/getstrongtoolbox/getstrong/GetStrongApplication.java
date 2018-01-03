e8c9a4d0492d68f3ac5e08783a63597297fe3799
Luke Buthman <lbuthman@gmail.com>
2018-01-03 07:31:16 -0600

    [chore] init commit

---
+++ src/main/java/com/getstrongtoolbox/getstrong/GetStrongApplication.java
@@ -0,0 +1,12 @@
+package com.getstrongtoolbox.getstrong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetStrongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetStrongApplication.class, args);
+	}
+}
