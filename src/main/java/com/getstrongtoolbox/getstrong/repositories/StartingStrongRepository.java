8b8430ea71f5080439bef775d0328ff5ea7b40a3
Luke Buthman <lbuthman@gmail.com>
2018-01-03 17:56:46 -0600

    [feat] Add repository for Starting Strong objects

---
+++ src/main/java/com/getstrongtoolbox/getstrong/repositories/StartingStrongRepository.java
@@ -0,0 +1,7 @@
+package com.getstrongtoolbox.getstrong.repositories;
+
+import com.getstrongtoolbox.getstrong.domain.StartingStrong;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StartingStrongRepository extends JpaRepository<StartingStrong, Long> {
+}
--- src/main/java/com/getstrongtoolbox/getstrong/respositories/StartingStrongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.getstrongtoolbox.getstrong.respositories;
-
-import com.getstrongtoolbox.getstrong.domain.Workout;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Phase1DayARepository extends JpaRepository<Workout, Long> {
-}
