5e7c40f49cf04a0c6b582644d789dfd717feb8fb
Luke Buthman <lbuthman@gmail.com>
2018-01-03 18:16:05 -0600

    [feat] Provide method to convert object to Json for testing

--- src/test/java/com/getstrongtoolbox/getstrong/web/rest/AbstractRestControllerTest.java
+++ src/test/java/com/getstrongtoolbox/getstrong/web/rest/AbstractRestControllerTest.java
@@ -1,4 +1,15 @@
 package com.getstrongtoolbox.getstrong.web.rest;
 
-public class AbstractRestControllerTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+abstract class AbstractRestControllerTest {
+
+    static String asJsonString(final Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        }
+        catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
 }
