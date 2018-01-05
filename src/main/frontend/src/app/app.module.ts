0704d135a6ae92e523216e4e95716c36af79a56c
Luke Buthman <lbuthman@gmail.com>
2018-01-05 04:57:18 -0600

    [feat] Init phase subcomponents

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { Phase1Component } from './program/starting-strong/phase1/phase1.compone
 import { Phase2Component } from './program/starting-strong/phase2/phase2.component';
 import { Phase3Component } from './program/starting-strong/phase3/phase3.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './program/starting-strong/phase1/form/form.component';
+import { ListComponent } from './program/starting-strong/phase1/list/list.component';
 
 
 @NgModule({
@@ -22,7 +24,9 @@ import { NavbarComponent } from './navbar/navbar.component';
     Phase1Component,
     Phase2Component,
     Phase3Component,
-    NavbarComponent
+    NavbarComponent,
+    FormComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
