ddc2076dbbd74793652dcb41daae92be84b58a2f
Luke Buthman <lbuthman@gmail.com>
2018-01-05 05:05:43 -0600

    [fix] Remove recently created components with name conflict

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -11,8 +11,6 @@ import { Phase1Component } from './program/starting-strong/phase1/phase1.compone
 import { Phase2Component } from './program/starting-strong/phase2/phase2.component';
 import { Phase3Component } from './program/starting-strong/phase3/phase3.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { FormComponent } from './program/starting-strong/phase1/form/form.component';
-import { ListComponent } from './program/starting-strong/phase1/list/list.component';
 
 
 @NgModule({
@@ -24,9 +22,7 @@ import { ListComponent } from './program/starting-strong/phase1/list/list.compon
     Phase1Component,
     Phase2Component,
     Phase3Component,
-    NavbarComponent,
-    FormComponent,
-    ListComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
