4c2a1c4f12c41c7c1e96cf66b843ce42342e32d0
Luke Buthman <lbuthman@gmail.com>
2018-01-07 11:17:50 -0600

    [feat] Use service for fetching workouts

--- src/main/frontend/src/app/program/starting-strong/phase1/phase1.component.ts
+++ src/main/frontend/src/app/program/starting-strong/phase1/phase1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {StartingStrengthWorkout} from "../startingStrengthWorkout";
+import {StartingStrongService} from "../../../services/starting-strong.service";
 
 @Component({
   selector: 'phase1',
@@ -19,34 +20,21 @@ export class Phase1Component implements OnInit {
     date: "01-01-18",
   };
 
-  workouts: StartingStrengthWorkout[] = [
-    {
-      id: 1,
-      squat: 135,
-      press: 135,
-      benchPress: 135,
-      deadLift: 135,
-      powerClean: 135,
-      pullUp: 135,
-      date: "01-01-18",
-    },
-    {
-      id: 1,
-      squat: 135,
-      press: 135,
-      benchPress: 135,
-      deadLift: 135,
-      powerClean: 135,
-      pullUp: 135,
-      date: "01-01-18",
-    },
-  ];
+  workouts: StartingStrengthWorkout[];
 
   phase: String = "phase1";
 
-  constructor() { }
+  constructor(private service: StartingStrongService) { }
 
   ngOnInit() {
+    this.getWorkouts();
+  }
+
+  getWorkouts(): void {
+    this.service.getWorkouts()
+      .subscribe(workouts => this.workouts = workouts,
+        (error) => console.log(error)
+      );
   }
 
 }
