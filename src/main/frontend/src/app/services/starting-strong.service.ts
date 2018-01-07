e9620818d6ae337d93579e5599f07b4950be10db
Luke Buthman <lbuthman@gmail.com>
2018-01-07 16:11:40 -0600

    [feat] Add emitter to service

--- src/main/frontend/src/app/services/starting-strong.service.ts
+++ src/main/frontend/src/app/services/starting-strong.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import {Observable} from "rxjs/Observable";
+import {StartingStrengthWorkout} from "../program/starting-strong/startingStrengthWorkout";
 
 @Injectable()
 export class StartingStrongService {
 
   private url = "/api/v1/starting-strong";
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  onWorkoutAdded = new EventEmitter<StartingStrengthWorkout>();
 
   constructor(private http: HttpClient) { }
 
@@ -15,5 +22,14 @@ export class StartingStrongService {
 
   getMostRecentWorkout(): Observable<any> {
     return this.http.get(this.url + "/most-recent");
+  }
+
+  addWorkout(workout: StartingStrengthWorkout) {
+    return this.http.post(this.url, workout, this.httpOptions);
+  }
+
+  deleteWorkout(id: number) {
+    let url = this.url + "/" + id;
+    return this.http.delete(url);
   }
 }
